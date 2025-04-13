e7ffc68b8f5f15ba36cdd13d203476aa83b04b53
princerwagasore <princealexanderrwagasore@gmail.com>
2025-04-13 04:42:21 -0700

    Add files via upload

---
+++ string.php
@@ -0,0 +1,21 @@
+g<!DOCTYPE html>
+<head>
+
+  
+    
+</head>
+<body>
+    <?php
+$sentence = "the main body of a book";
+
+echo strtolower($sentence);
+
+echo strtoupper($sentence);
+echo str_replace("book", "college",$sentence);
+echo strlen("$sentence");
+echo $sentence [6] ="6"
+?>
+
+
+</body>
+</html>
